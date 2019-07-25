18a2933268c5b8f59592bf3df42df9c3241b5763
Ayush Pratap <ayushs56@gmail.com>
2019-07-25 09:26:30 +0530

    Placing the tcp-http-client code in this folder

---
+++ tcp_http client/clients.js
@@ -0,0 +1,107 @@
+/*
+    Filename: clients.js
+    Description:
+ */
+//------------------------------------------------------------------------------
+require('magic-globals');
+const express       = require('express');
+const net           = require('net');
+const request         = require('request');
+var CONFIG          = require('./config/config');
+const JsonSocket    = require('json-socket');
+const logger        = CONFIG.logger;
+var options         = {};
+//------------------------------------------------------------------------------
+console.log(CONFIG.tcp_server_host);
+console.log(CONFIG.tcp_server_port);
+// Create client socket
+logger.info('[%s] , Creating client socket',__file);
+const tcpClient = net.createConnection(CONFIG.tcp_server_port,CONFIG.tcp_server_host);
+
+//  Handle connect event
+tcpClient.on('connect',function(){
+    logger.info("[%s] , Connected to the server at %s:%s",__file,CONFIG.tcp_server_host,CONFIG.tcp_server_port);
+    logger.info("[%s] , TCP client info %s",__file,tcpClient.address().address);
+});
+
+//  Handle data event
+tcpClient.on('data',function(data){
+    logger.info('[%s] , Data recevied',__file);
+
+    // Convert the Buffer to JSON object
+    let reqInfo = JSON.parse(data.toString());
+    logger.debug('[%s] , Request info : %o ',__file,reqInfo);
+    // Get the route of the request
+    switch(reqInfo.route){
+        case '/':
+            logger.info("[%s] , Case : '/'",__file);
+            
+            // Set the options for HTTPS request
+            options.method              = "GET";
+            options.url                 = CONFIG.client_service_url+reqInfo.route;
+            options.rejectUnauthorized  = false;
+            options.requestCert         = false;
+            options.body                = reqInfo.data;
+
+            logger.debug('[%s] , HTTPS request options : %o',__file,options);
+            
+            // Make HTTPS request
+            request(options,function(error,res,body){
+                logger.info('[%s] , Response received for HTTPS request',__file);
+                options = {};
+                logger.debug('[%s] , Cleared HTTPS options : %o ',__file,options);
+                logger.info('[%s] , Response body : %o ',__file,body);
+            });
+        break;
+        case '/api/makeCall':
+            logger.info("[%s] , Case : /api/makeCall",__file);
+
+            // Set the options for HTTPS request
+            options.method              = "POST";
+            options.url                 = CONFIG.client_service_url+reqInfo.route;
+            options.json                = true;
+            options.rejectUnauthorized  = false;
+            options.requestCert         = false;
+            options.body                = reqInfo.data;
+
+            logger.debug('[%s] , HTTPS request options : %o',__file,options);
+
+            //  Make HTTPS request
+            request(options,function(error,res,body){
+                logger.info('[%s] , Response received for HTTPS request',__file);
+                options = {};
+                logger.debug('[%s] , Cleared HTTPS options : %o',__file,options);
+                logger.info('[%s] , Request body : %o ',__file,body);
+            });
+        break;
+        case '/api/makeCallMulti':
+            logger.info("[%s] , Case : /api/makeCallMulti",__file);
+
+            //  Set the options for HTTPS request
+            options.method              = "POST";
+            options.url                 = CONFIG.client_service_url+reqInfo.route;
+            options.json                = true;
+            options.rejectUnauthorized  = false;
+            options.requestCert         = false;
+            options.body                = reqInfo.data;
+
+            logger.debug('[%s] , HTTPS request options : %o',__file,options);
+
+            // Make HTTPS request
+            request(options,function(error,res,body){
+                logger.info('[%s] , Response received for HTTPS request',__file);
+                options = {};
+                logger.debug('[%s] , Cleared HTTPS options : %o',__file,options);
+                logger.info('[%s] , Response body : %o',__file,body);
+            });
+         break;
+        default:
+            logger.error('[%s] , No matching case',__file);
+    }
+});
+
+//  Handle error event
+tcpClient.on('error',function(err){
+    logger.error('[%s] , Error : %o',__file,err);
+});
+//------------------------------------------------------------------------------
---
+++ tcp_http client/config/config.js
@@ -0,0 +1,85 @@
+//------------------------------------------------------------------------------
+require('dotenv').config();
+var appRoot 		= require('app-root-path');
+const winston 		= require('winston');
+let CONFIG 			= {};
+//------------------------------------------------------------------------------
+//	Add parameters to the global CONFIG
+//------------------------------------------------------------------------------
+CONFIG.app 					= process.env.APP 					|| 'dev';
+CONFIG.tcp_server_port 		= process.env.TCP_SERVER_PORT 		|| '9001';
+CONFIG.tcp_server_host 		= process.env.TCP_SERVER_HOST 		|| 'ec2-3-80-153-122.compute-1.amazonaws.com';
+CONFIG.client_service_url 	= process.env.CLIENT_SERVICE_URL 	|| 'https://127.0.0.1:8443';
+CONFIG.env 					= process.env.ENV					|| 'dev';
+//------------------------------------------------------------------------------
+// Create logger for logging
+// -----------------------------------------------------------------------------
+const logger = winston.createLogger({
+		transports:[
+		new winston.transports.File({
+										format: winston.format.combine(
+												winston.format.label({
+														label: CONFIG.env
+													}),												
+												winston.format.timestamp({
+													format: 'HH:mm:ss DD-YYYY-MM'
+												}),
+												winston.format.splat(),
+												winston.format.json(),
+												winston.format.prettyPrint(),
+												winston.format.printf(error => `${error.timestamp} ${error.level}: ${error.message}`)
+											),
+										filename: appRoot+'/logs/error.log', 
+										level: 'error'
+									}),
+		new winston.transports.File({
+										format: winston.format.combine(
+												winston.format.label({
+														label: CONFIG.env
+													}),
+												winston.format.timestamp({
+													format: 'HH:mm:ss DD-YYYY-MM'
+												}),
+												winston.format.splat(),
+												winston.format.json(),
+												winston.format.prettyPrint(),
+												winston.format.printf(
+													info => `${info.timestamp} ${info.level}: ${info.message}`,
+													debug => `${debug.timestamp} ${debug.level}: ${debug.message}`,
+													error => `${error.timestamp} ${error.level}: ${error.message}`)
+											),										
+										filename: appRoot+'/logs/combined.log',
+										level: 'silly'
+									})
+	]
+});
+
+// If application is running in dvelopemnt environment then show logs to console
+// else do not show logs on console
+if(process.env.ENV === 'prod'){
+	logger.add(new winston.transports.Console({
+		format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.splat(),
+				winston.format.simple()
+			),
+		level: 'info'
+	}));
+}
+
+if(process.env.ENV === 'dev'){
+	logger.add(new winston.transports.Console({
+		format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.splat(),
+				winston.format.simple()
+			),
+		level: 'silly'
+	}));
+}
+//------------------------------------------------------------------------------
+// Add the logger instance to the global CONFIG
+//------------------------------------------------------------------------------
+CONFIG.logger = logger;
+//------------------------------------------------------------------------------
+module.exports = CONFIG;
