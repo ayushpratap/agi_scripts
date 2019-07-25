238c89ab7c5688fdfb50e4e6d5eeff066ff2f8cc
Ayush Pratap <ayushs56@gmail.com>
2019-07-25 09:28:37 +0530

    Removing non existing files

--- grant_token.sh
+++
@@ -1,3 +0,0 @@
-
-
-curl -L -H "application/x-www-form-urlencoded" -d 'grant_type=authorization_code&code=ANIfBExTSfdKSOJHvpaR&client_id=amzn1.application-oa2-client.c899b6675aaa44b59d650b9f5b793a4b&client_secret=69180e19aa89b0ee655177639ff9ca47e7155d2c1a0b69ce74c29cd358d47ad8&redirect_uri=https%3A%2F%2Flocalhost' https://api.amazon.com/auth/o2/token
